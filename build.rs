f88b10feeb3a0251013453161f2c0c8971f04c49
Alex Huszagh <ahuszagh@gmail.com>
2021-04-27 10:50:30 -0500

    Bug fixes for build scripts, thanks to dtolnay.

--- build.rs
+++ build.rs
@@ -9,12 +9,13 @@ fn main() {
     // See `lexical-core/src/atof/algorithm/math.rs` for detailed
     // instructions of architecture instruction support for 64-bit
     // mathematical operations.
-    let limb_width_64 = cfg!(any(
-        target_arch = "aarch64",
-        target_arch = "mips64",
-        target_arch = "powerpc64",
-        target_arch = "x86_64"
-    ));
+
+    // https://github.com/rust-lang/cargo/issues/4302#issuecomment-316482399
+    let limb_64_archs = ["aarch64", "mips64", "powerpc64", "x86_64"];
+    let limb_width_64 = match std::env::var("CARGO_CFG_TARGET_ARCH") {
+        Ok(arch) => limb_64_archs.contains(&&*arch),
+        _ => false,
+    };
     if limb_width_64 {
         println!("cargo:rustc-cfg=limb_width_64");
     } else {
